2786be62eb2227c179d485f4bfeebf9500d757f9
Martin Burdon <martin@martinburdon.co.uk>
2017-10-22 22:26:25 +0100

    Added data

---
+++ client/data/defaultState.js
@@ -0,0 +1,21 @@
+const state = {
+  gallery: [
+    {
+      image: '/some-url.jpg',
+      title: 'Tasty cakes',
+      description: 'Treat yourself!',
+      tags: [
+        'cupcakes',
+        'christmas',
+        'animals',
+      ]
+    },
+    {
+      ...
+    }
+  ],
+  visibilityFilter: [
+    'cupcakes',
+    ...
+  ]
+}
